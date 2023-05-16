df8df989083a39b7376f291eebea30b2689c86b5
Michał Gapiński <mike@gapinski.eu>
2023-05-16 19:34:11 +0200

    Add CI job for AOSP

--- dsl.groovy
+++ dsl.groovy
@@ -3,7 +3,8 @@ GITHUB_ORG_NAME = 'tesla-android'
 FLUTTER_APP = 'flutter-app'
 FLUTTER_APP_CI_JOB_NAME = FLUTTER_APP + ' (CI)'
 FLUTTER_APP_RELEASE_JOB_NAME = FLUTTER_APP + ' (release and tag)'
-
+ANDROID_RPI4 = 'android-raspberry-pi'
+ANDROID_RPI4_CI_JOB_NAME = ANDROID_RPI4 + ' (CI)'
 
 folder(ROOT_PROJECT) {
     description('Folder containing all jobs for ' + ROOT_PROJECT)
@@ -12,7 +13,7 @@ folder(ROOT_PROJECT) {
 multibranchPipelineJob(ROOT_PROJECT + '/' + FLUTTER_APP_CI_JOB_NAME) {
     branchSources {
         github {
-        	id(ROOT_PROJECT + '/' + FLUTTER_APP_CI_JOB_NAME)
+            id(ROOT_PROJECT + '/' + FLUTTER_APP_CI_JOB_NAME)
             repoOwner(GITHUB_ORG_NAME)
             repository(FLUTTER_APP)
             scanCredentialsId('tesla-android-jenkins')
@@ -42,6 +43,33 @@ pipelineJob(ROOT_PROJECT + '/' + FLUTTER_APP_RELEASE_JOB_NAME) {
     definition {
         cps {
         	script(readFileFromWorkspace('flutter-app-release-and-tag.groovy'))
+        }
+    }
+}
+
+multibranchPipelineJob(ROOT_PROJECT + '/' + ANDROID_RPI4_CI_JOB_NAME) {
+    branchSources {
+        github {
+            id(ROOT_PROJECT + '/' + FLUTTER_APP_CI_JOB_NAME)
+            repoOwner(GITHUB_ORG_NAME)
+            repository(ANDROID_RPI4)
+            scanCredentialsId('tesla-android-jenkins')
+            includes('*')
+        }
+    }
+    
+    triggers {
+        cron('*/5 * * * *')
+    }
+    orphanedItemStrategy {
+        discardOldItems { 
+            daysToKeep(90)
+            numToKeep(180)
+        }
+    }
+    factory {
+        workflowBranchProjectFactory {
+            scriptPath('jenkins/multi-branch-ci.groovy')
         }
     }
 }
